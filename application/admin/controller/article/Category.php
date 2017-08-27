3eb4ea7282cb02d901ad1bc125dcc94d91ae1f69
klaus <2511634167@qq.com>
2017-08-27 22:59:59 +0800

    分类增加

--- application/admin/controller/article/Category.php
+++ application/admin/controller/article/Category.php
@@ -25,19 +25,33 @@ class Category extends Admin
      */
     public function add()
     {
-        $request = request();
-        url();
-
         // 显示增加页面
-        if ($request->isGet())
-        {
+        if ($this->request->isGet()) {
             return view();
         }
 
         // 增加分类
-        if ($request->isAjax() && $request->isPost())
-        {
+        if ($this->request->isPost()) {
+            $name = $this->request->param('name', '');
+            $pid  = $this->request->param('pid', 0, '\intval');
 
+            if (!$name) {
+                return json_encode(
+                    [
+                        'status' => 400,
+                        'info'   => '名称不能为空'
+                    ]
+                );
+            }
+
+            $categoryAdd = Db::name('article_type')->insert(
+                [
+                    'name'     => $name,
+                    'pid'      => $pid,
+                    'add_time' => date('Y-m-d H:i:s'),
+                ]
+            );
+            dump($categoryAdd);
         }
 
     }
