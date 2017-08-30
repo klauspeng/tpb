58ff8ee10ba68cd4558e979b841b365dc63e880b
klauspeng <2511634167@qq.com>
2017-08-30 14:05:41 +0800

    PDO持久化连接

--- application/database.php
+++ application/database.php
@@ -25,7 +25,10 @@ return [
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
-    'params'          => [],
+    'params'          => [
+        // 持久化连接
+        \PDO::ATTR_PERSISTENT => true,
+    ],
     // 数据库编码默认采用utf8
     'charset'         => 'utf8',
     // 数据库表前缀
