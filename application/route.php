000110ab8d7e0abd2869f76e9df564ce04f33eb0
klauspeng <2511634167@qq.com>
2017-09-11 18:33:06 +0800

    绑定木块，及文章显示优化

--- application/route.php
+++ application/route.php
@@ -6,11 +6,11 @@ return [
         'id'   => '\d+',
     ],
     // 文章分类列表
-    'admin/article/category/index' => ['admin/article.category/index', ['method' => 'get|post']],
-    'admin/article/category/add'   => ['admin/article.category/add', ['method' => 'get|post']],
-    'admin/article/category/del'   => ['admin/article.category/del', ['method' => 'get|post']],
+    'article/category/index' => ['admin/article.category/index', ['method' => 'get|post']],
+    'article/category/add'   => ['admin/article.category/add', ['method' => 'get|post']],
+    'article/category/del'   => ['admin/article.category/del', ['method' => 'get|post']],
     // 文章
-    'admin/article/article/index'  => ['admin/article.article/index', ['method' => 'get|post']],
-    'admin/article/article/add'    => ['admin/article.article/add', ['method' => 'get|post']],
-    'admin/article/article/del'    => ['admin/article.article/del', ['method' => 'get|post']],
+    'article/article/index'  => ['admin/article.article/index', ['method' => 'get|post']],
+    'article/article/add'    => ['admin/article.article/add', ['method' => 'get|post']],
+    'article/article/del'    => ['admin/article.article/del', ['method' => 'get|post']],
 ];
